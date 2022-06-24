c1d438ac4ecb01706bacb10f817b014cb6552b14
Alexandr Timoschuk <app.gelios@gmail.com>
2022-06-24 21:17:37 +0300

    Superheroes

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:superheroes/pages/main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,// If this is not set, then ThemeData.light().textTheme is used
+         ),
+      ),
+      home: MainPage(),
+    );
+  }
+}
